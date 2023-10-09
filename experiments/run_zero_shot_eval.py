c09e2dfb0407f363a56580a0c16509672494688d
Max Croci <t-mcroci@microsoft.com>
2023-10-09 17:39:27 +0000

    Add base LM class.

--- experiments/run_zero_shot_eval.py
+++ experiments/run_zero_shot_eval.py
@@ -1,7 +1,108 @@
 import argparse
 import json
-
+import torch
 from lm_eval import tasks, evaluator, utils
+from lm_eval.base import BaseLM
+from transformers import OPTForCausalLM, AutoTokenizer
+
+class OPTClass(BaseLM):
+    def __init__(self, args, **kwargs):
+        # TODO: simplify args and add SliceGPT logic.
+        super().__init__()
+
+        class OPTArgs:
+            def __init__(self):
+                self.model = None
+                self.batch_size = 1
+                self.eval_dataset = 'wikitext2'
+                self.seed = 0
+                self.sparsity = None
+                self.cal_nsamples = 128
+                self.seqlen = None
+                self.compress_head = False
+                self.load_dir = None
+
+
+        args = OPTArgs()
+
+        self.args = args
+        self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model_name = kwargs.get("model")
+
+        self.batch_size_per_gpu = args.batch_size
+
+        self.model = OPTForCausalLM.from_pretrained(self.model_name, torch_dtype='auto').to(self._device)
+        self.model.eval()
+        self.seqlen = self.model.config.max_position_embeddings
+        self.args.seqlen = self.seqlen
+        self.args.model = self.model_name
+        self.args.sparsity = kwargs.get("sparsity")
+        
+        # rotate model
+        self.rotate()
+
+        # pretrained tokenizer for neo is broken for now so just hard-coding this to gpt2
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, use_fast=False)
+        self.vocab_size = self.tokenizer.vocab_size
+        print('OPT vocab size: ', self.vocab_size)
+
+    def rotate(self):
+        # TODO
+        pass
+
+    @classmethod
+    def create_from_arg_string(cls, args, kwargs):
+        return cls(args, kwargs)
+
+    @property
+    def eot_token_id(self):
+        # we use EOT because end of *text* is more accurate for what we're doing than end of *sentence*
+        return self.tokenizer.eos_token_id
+
+    @property
+    def max_length(self):
+        try:
+            return self.gpt2.config.n_ctx
+        except AttributeError:
+            # gptneoconfig doesn't have n_ctx apparently
+            return self.model.config.max_position_embeddings
+    
+    @property
+    def max_gen_toks(self):
+        print('max_gen_toks fn')
+        return 256
+
+    @property
+    def batch_size(self):
+        # TODO: fix multi-gpu
+        return self.batch_size_per_gpu  # * gpus
+
+    @property
+    def device(self):
+        # TODO: fix multi-gpu
+        return self._device
+
+    def tok_encode(self, string: str):
+        return self.tokenizer.encode(string, add_special_tokens=False)
+
+    def tok_decode(self, tokens):
+        return self.tokenizer.decode(tokens)
+
+    def _model_call(self, inps):
+        """
+        inps: a torch tensor of shape [batch, sequence]
+        the size of sequence may vary from call to call
+        returns: a torch tensor of shape [batch, sequence, vocab] with the
+        logits returned from the model
+        """
+        with torch.no_grad():
+            return self.model(inps)[0][:, :, :50272]
+        
+    def _model_generate(self, context, max_length, eos_token_id):
+        return self.model.generate(
+            context, max_length=max_length, eos_token_id=eos_token_id, do_sample=False
+        )
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -15,10 +116,13 @@ def parse_args():
 
     return parser.parse_args()
 
-
 def main():
     args = parse_args()
 
+    ### SliceGPT ###
+    my_model = OPTClass(args, model="facebook/opt-1.3b", sparsity=0.0)
+
+    ### LM Eval Harness ###
     if args.tasks is None:
         task_names = tasks.ALL_TASKS
     else:
@@ -27,7 +131,7 @@ def main():
     print(f"Selected Tasks: {task_names}")
 
     results = evaluator.simple_evaluate(
-        model=args.model,
+        model=my_model,
         model_args=args.model_args,
         tasks=task_names,
         num_fewshot=args.num_fewshot,
@@ -38,7 +142,6 @@ def main():
     print(dumped)
 
     print(evaluator.make_table(results))
-
 
 if __name__ == "__main__":
     main()
