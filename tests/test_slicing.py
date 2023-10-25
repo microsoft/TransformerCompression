c792f46e1d05e824994b6ad129d63ca3385e3b8d
Liana Mikaelyan <REDMOND.t-lmikaelyan@GCRSANDBOX531.redmond.corp.microsoft.com>
2023-10-25 14:49:54 +0000

    Add comment to test

--- tests/test_slicing.py
+++ tests/test_slicing.py
@@ -5,6 +5,7 @@ from slicegpt import hf_utils, layernorm_fusion
 
 
 def test_layernorm_fusion_replaces_modules():
+    """ Checks that module parameters are changes after applying layernorm fusion """
     model_name = "facebook/opt-125m"
     model, tokenizer = hf_utils.get_model(model_name)
 
