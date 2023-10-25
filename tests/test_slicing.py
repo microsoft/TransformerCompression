19816771b6b00ec5bee8b3a3304651a0ec320582
Liana Mikaelyan <REDMOND.t-lmikaelyan@GCRSANDBOX531.redmond.corp.microsoft.com>
2023-10-25 13:44:02 +0000

    Fix test

--- tests/test_slicing.py
+++ tests/test_slicing.py
@@ -4,16 +4,15 @@
 from slicegpt import hf_utils, layernorm_fusion
 
 
-def test_layernorm_fusion():
+def test_layernorm_fusion_replaces_modules():
     model_name = "facebook/opt-125m"
     model, tokenizer = hf_utils.get_model(model_name)
 
-    layernorm_fusion.replace_modules(model, model.config)
     orig_layers = get_layers(model)
 
-    assert orig_layers != get_layers(model)
-
+    layernorm_fusion.replace_modules(model, model.config)
     layernorm_fusion.fuse_modules(model)
+    
     assert orig_layers != get_layers(model)
 
 def get_layers(model):
