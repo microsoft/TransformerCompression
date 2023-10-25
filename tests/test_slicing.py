999ffd27b15195ef2e6be2948e8bb2d34aab7c5a
Liana Mikaelyan <REDMOND.t-lmikaelyan@GCRSANDBOX531.redmond.corp.microsoft.com>
2023-10-25 13:33:48 +0000

    Update layerfusion test

--- tests/test_slicing.py
+++ tests/test_slicing.py
@@ -8,13 +8,13 @@ def test_layernorm_fusion():
     model_name = "facebook/opt-125m"
     model, tokenizer = hf_utils.get_model(model_name)
 
+    layernorm_fusion.replace_modules(model, model.config)
     orig_layers = get_layers(model)
 
-    layernorm_fusion.replace_modules(model, model.config)
-    assert orig_layers == get_layers(model)
+    assert orig_layers != get_layers(model)
 
     layernorm_fusion.fuse_modules(model)
-    assert orig_layers == get_layers(model)
+    assert orig_layers != get_layers(model)
 
 def get_layers(model):
-    return [name for name, _ in model.named_children()]
+    return [name for name, _ in model.named_parameters()]
