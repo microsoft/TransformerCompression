035019dc2f6a40879ad2f1bfbff8ecd1110c90a1
Max Croci <t-mcroci@microsoft.com>
2023-11-11 20:39:26 +0000

    Update benchmarking.

--- experiments/run_benchmark.sh
+++ experiments/run_benchmark.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 RUN=true
+DISTRIBUTE=true
 
 PYTHON_SCRIPT="run_benchmark.py"
 
-MODEL_FAMILY="meta-llama"
-MODEL_NAME="Llama-2-7b-hf"
+#MODEL_FAMILY="meta-llama"
+#MODEL_NAME="Llama-2-7b-hf"
 
-#MODEL_FAMILY="facebook"
-#MODEL_NAME="opt-125m"
+MODEL_FAMILY="facebook"
+MODEL_NAME="opt-125m"
 
 MODEL="${MODEL_FAMILY}/${MODEL_NAME}"
 BASE_ARGS="--model=$MODEL --hf_token=***REMOVED***"
+if $DISTRIBUTE; then
+    BASE_ARGS="$BASE_ARGS --distribute_model"
+fi
 
-BATCH_SIZE_DEN_MODEL=512
-BATCH_SIZE_SLI_MODEL=1024
+BATCH_SIZE_DEN_MODEL=256
+BATCH_SIZE_SLI_MODEL=256
 SPARSITY=0.25
 
-# Add 128 to the batch size for the denominator model to account for the
-# additional memory required for the fused model.
-for TEST_ID in {0..2}; do
-
-    for GPU_ID in {0..1}; do
+if $DISTRIBUTE; then
+    for TEST_ID in {0..2}; do
+        GPU_ID="0,1"
         ARGS="$BASE_ARGS --batch_size=$BATCH_SIZE_DEN_MODEL"
         echo $GPU_ID : $PYTHON_SCRIPT $ARGS
         if $RUN; then
@@ -28,10 +30,9 @@ for TEST_ID in {0..2}; do
             sleep 5
         fi
         
-        BATCH_SIZE_DEN_MODEL=$((BATCH_SIZE_DEN_MODEL + 128))
-    done
+        BATCH_SIZE_DEN_MODEL=$((BATCH_SIZE_DEN_MODEL * 2))
 
-    for GPU_ID in {2..3}; do
+        GPU_ID="2,3"
         LOAD_MODEL_PATH="/data/sliced_models/${MODEL_NAME}_${SPARSITY}.pt"
         ARGS="$BASE_ARGS --batch_size=$BATCH_SIZE_SLI_MODEL --sparsity=$SPARSITY --load_model_path=$LOAD_MODEL_PATH"
         echo $GPU_ID : $PYTHON_SCRIPT $ARGS
@@ -41,9 +42,41 @@ for TEST_ID in {0..2}; do
             sleep 5
         fi
         
-        BATCH_SIZE_SLI_MODEL=$((BATCH_SIZE_SLI_MODEL + 128))
+        BATCH_SIZE_SLI_MODEL=$((BATCH_SIZE_SLI_MODEL * 2))
+
+        wait
+        echo "Completed test $TEST_ID."
     done
 
-    wait
-    echo "Completed test $TEST_ID."
-done
+else
+    for TEST_ID in {0..2}; do
+        for GPU_ID in {0..1}; do
+            ARGS="$BASE_ARGS --batch_size=$BATCH_SIZE_DEN_MODEL"
+            echo $GPU_ID : $PYTHON_SCRIPT $ARGS
+            if $RUN; then
+                CUDA_VISIBLE_DEVICES=$GPU_ID python $PYTHON_SCRIPT $ARGS &
+                sleep 5
+            fi
+            
+            BATCH_SIZE_DEN_MODEL=$((BATCH_SIZE_DEN_MODEL + 128))
+        done
+
+        for GPU_ID in {2..3}; do
+            LOAD_MODEL_PATH="/data/sliced_models/${MODEL_NAME}_${SPARSITY}.pt"
+            ARGS="$BASE_ARGS --batch_size=$BATCH_SIZE_SLI_MODEL --sparsity=$SPARSITY --load_model_path=$LOAD_MODEL_PATH"
+            echo $GPU_ID : $PYTHON_SCRIPT $ARGS
+            
+            if $RUN; then
+                CUDA_VISIBLE_DEVICES=$GPU_ID python $PYTHON_SCRIPT $ARGS &
+                sleep 5
+            fi
+            
+            BATCH_SIZE_SLI_MODEL=$((BATCH_SIZE_SLI_MODEL + 128))
+        done
+
+        wait
+        echo "Completed test $TEST_ID."
+    done
+fi
+
+echo "Completed all tests."
