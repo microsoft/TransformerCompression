b4f1c12b410a035ee0fca80d80c5b73ab6d0fc80
James Hensman <jameshensman@microsoft.com>
2023-11-24 16:33:18 +0000

    isort fix.

--- experiments/run_zero_shot_tasks.py
+++ experiments/run_zero_shot_tasks.py
@@ -7,11 +7,11 @@ import logging
 import os
 
 import torch
+import wandb
 from lm_eval import evaluator, tasks
 from lm_eval import utils as lm_eval_utils
 from lm_eval.base import BaseLM
 
-import wandb
 from slicegpt import data_utils, gpu_utils, hf_utils, layernorm_fusion, rotate, utils
 from slicegpt.config import config
 
