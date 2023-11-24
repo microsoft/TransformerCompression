3b159f7d6d5a7d70dfb0e248e7154bd57913d920
Dmitry Kats <dmitrykats@microsoft.com>
2023-11-24 11:23:40 +0000

    Ignore type errors in gpu_utils instead of explicit casting

--- src/slicegpt/gpu_utils.py
+++ src/slicegpt/gpu_utils.py
@@ -27,13 +27,13 @@ def evaluate_ppl(model: ModelAdapter, testloader: DataLoader[Tensor]) -> float:
 
     model.raw_model.eval()
 
-    loss_fct = cast(Callable[[Tensor, Tensor], Tensor], torch.nn.CrossEntropyLoss(reduction="none"))
+    loss_fct = torch.nn.CrossEntropyLoss(reduction="none")
 
     nlls = []
 
     for batch in testloader:
         assert isinstance(batch, Tensor)
-        input_ids = batch.to(cast(torch.device, config.device))
+        input_ids = batch.to(config.device)  # type: ignore
         logits: Tensor = model.compute_output_logits(input_ids=input_ids)
 
         # Shift outputs and labels autoregressively.
