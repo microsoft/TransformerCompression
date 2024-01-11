f83f93edea88707e39dc820c2d0cf720a4b3d854
root <jameshensman@microsoft.com>
2024-01-11 11:23:05 +0000

    initial go at docstrings

--- src/slicegpt/model_adapter.py
+++ src/slicegpt/model_adapter.py
@@ -46,30 +46,54 @@ class LayerAdapter(ABC):
 
     @abstractmethod
     def get_first_layernorm(self) -> Module:
+        """
+        Returns the first layer norm in the layer, usually the one before the attention component.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def get_second_layernorm(self) -> Module:
+        """
+        Returns the second layer norm in the layer, usually the one before the MLP component.
+        In the case where the layer has only one LayerNorm, should raise an exception.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def get_attention_inputs(self) -> Sequence[Linear]:
+        """
+        Returns a list of the Linear layers (nn.modules) that are inputs to the attention component.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def get_attention_output(self) -> Linear:
+        """
+        returns the Linear layer (nn.module) that is the output of the attention component.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def get_mlp_inputs(self) -> Sequence[Linear]:
+        """
+        Returns a list of the Linear layers (nn.modules) that are inputs to the MLP component.
+
+        For simple mlps, this will be a list of length 1. For gated mlps (as in the Llama models) there will be two.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def get_mlp_output(self) -> Linear:
+        """
+        Returns the Linear layer (nn.module) that is the output of the MLP component (usually fc2 or down_proj)
+        """
         raise NotImplementedError
 
     def get_updated_args(self, hidden_states: Any, args: tuple) -> tuple:
-        """Returns a copy of args with updated hidden_states."""
+        """
+        `args` is a tuple of the arguments to the layer's forward method. hidden_states is the new value for the hidden_states argument.
+        This method returns a new tuple of arguments with the hidden_states argument updated.
+        """
         return (
             args[: self.hidden_states_args_position] + (hidden_states,) + args[self.hidden_states_args_position + 1 :]
         )
@@ -79,41 +103,70 @@ class ModelAdapter(ABC):
     @property
     @abstractmethod
     def parallel_blocks(self) -> bool:
+        """
+        Whether the model has parallel attention and mlp blocks (True in phi2) or sequential (False in llama).
+        """
         raise NotImplementedError
 
     @property
     @abstractmethod
     def model(self) -> Module:
+        """
+        The base model that slicegpt interacts with.
+        """
         raise NotImplementedError
 
     @property
     @abstractmethod
     def no_split_module_classes(self) -> list[str] | None:
+        """
+        A list of string specifying the names of modules that should not be split.
+
+        See https://huggingface.co/docs/accelerate/concept_guides/big_model_inference for more details.
+        """
         raise NotImplementedError
 
     @property
     @abstractmethod
     def seqlen(self) -> int:
+        """
+        The (maximum) sequence length of the model
+        """
         raise NotImplementedError
 
     @property
     @abstractmethod
     def hidden_size(self) -> int:
+        """
+        The hidden size of the model
+        """
         raise NotImplementedError
 
     @property
     @abstractmethod
     def should_bake_mean_into_linear(self) -> bool:
+        """
+        Whether the models normalization layers (e.g. Layernorm) contain a mean-subtraction
+        operation that neds to be absorbed into previous linear layers.
+
+        For LayerNorm, this is True. For RMSNorm, this is False.
+        """
         raise NotImplementedError
 
     @property
     @abstractmethod
     def original_layer_type(self) -> type:
+        """
+        The class of the layer that is being compressed (so that we can replace it with a compressed version)
+        """
         raise NotImplementedError
 
     @property
     @abstractmethod
     def original_layer_norm_type(self) -> type:
+        """
+        The class of the LayerNorm (or equivalent) in the original model, so that we can replace it with RMSNorm (needed for computational invariance).
+        """
         raise NotImplementedError
 
     @property
@@ -129,34 +182,59 @@ class ModelAdapter(ABC):
 
     @abstractmethod
     def compute_output_logits(self, input_ids: Tensor) -> FloatTensor:
+        """
+        returns the logits for the model on the given input_ids. For example, this might look like:
+            `self._model(input_ids=input_ids).logits`
+        """
         raise NotImplementedError
 
     @abstractmethod
     def convert_layer_to_compressible(self, layer: Module) -> Module:
+        """
+        Replace the given layer with a compressed version of the layer.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def get_layers(self) -> Sequence[LayerAdapter]:
+        """
+        Returns a list of LayerAdapters, one for each layer in the model.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def get_raw_layer_at(self, index: int) -> Module:
+        """
+        Returns the raw layer (no adpter) at the given index.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def set_raw_layer_at(self, index: int, new_layer: Module) -> None:
+        """
+        Assigns the given layer to the model at the given index.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def get_embeddings(self) -> list[Module]:
+        """
+        Returns a list of the embedding modules in the model.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def get_pre_head_layernorm(self) -> Module:
+        """
+        Returns the layer norm (or equivalent) before the head of the model.
+        """
         raise NotImplementedError
 
     @abstractmethod
     def get_lm_head(self) -> Linear:
+        """
+        Returns the linear layer at the head of the model (usually of size hidden-ize x vocab-size)
+        """
         raise NotImplementedError
 
     @final
