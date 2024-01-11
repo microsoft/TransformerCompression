a84655d11f5fad28b5f4ee06c37cb5ed7a52a4d5
Dmitry Kats <dmitrykats@microsoft.com>
2024-01-11 16:13:30 +0000

    Initial public api

--- src/slicegpt/__init__.py
+++ src/slicegpt/__init__.py
@@ -1,4 +1,19 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-from . import data_utils, gpu_utils, rotate
+from .model_adapter import LayerAdapter, ModelAdapter
+from .adapters.llama_adapter import LlamaModelAdapter
+from .adapters.opt_adapter import OPTModelAdapter
+from .data_utils import get_dataset, prepare_dataloader
+from .gpu_utils import benchmark, evaluate_ppl, distribute_model
+from .hf_utils import get_model_and_tokenizer, load_sliced_model
+from .layernorm_fusion import fuse_modules, replace_layers
+from .rotate import rotate_and_slice
+
+__all__ = [
+    "data_utils",
+    "gpu_utils",
+    "hf_utils",
+    "layernorm_fusion",
+    "rotate"
+]
