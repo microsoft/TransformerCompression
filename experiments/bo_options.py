f2435bc542d4589ff8f65985ef4e6cf563ec3ca5
Pashmina Cameron <11311835+pashminacameron@users.noreply.github.com>
2024-01-25 15:53:59 +0000

    Raise an error for models without lora target map

--- experiments/bo_options.py
+++ experiments/bo_options.py
@@ -4,10 +4,10 @@ def lora_target_map(model: str):
     match model:
         case "microsoft/phi-2":
             return {
-                    "qkv_proj": "k_proj q_proj v_proj",
-                    "attn_head": "k_proj q_proj v_proj dense",
-                    "attn_head_and_mlp": "k_proj q_proj v_proj dense fc1 fc2",
-                    "attn_head_mlp_lm_head": "k_proj q_proj v_proj dense fc1 fc2 lm_head"
+                "qkv_proj": "k_proj q_proj v_proj",
+                "attn_head": "k_proj q_proj v_proj dense",
+                "attn_head_and_mlp": "k_proj q_proj v_proj dense fc1 fc2",
+                "attn_head_mlp_lm_head": "k_proj q_proj v_proj dense fc1 fc2 lm_head"
             }
         case "facebook/opt-125m" | "facebook/opt-1.3b" | \
              "facebook/opt-2.7b" | "facebook/opt-6.7b" | \
@@ -25,5 +25,7 @@ def lora_target_map(model: str):
                 "attn_head_and_mlp": "k_proj q_proj v_proj o_proj gate_proj up_proj down_proj",
                 "attn_head_mlp_lm_head": "k_proj q_proj v_proj o_proj gate_proj up_proj down_proj lm_head"
             }
+        case _:
+            raise RuntimeError(f"Lora target map undefined for model={model}")
 
 
