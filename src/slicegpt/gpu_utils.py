6150c62fc49308b9abe20d9997551b7d9017c905
Pashmina Cameron <11311835+pashminacameron@users.noreply.github.com>
2024-02-17 16:32:29 +0000

    Formatting

--- src/slicegpt/gpu_utils.py
+++ src/slicegpt/gpu_utils.py
@@ -16,7 +16,9 @@ from .model_adapter import ModelAdapter
 
 
 @torch.no_grad()
-def evaluate_ppl(model: torch.nn.Module, pad_token_id: int | None, testloader: DataLoader[dict[str, torch.Tensor]]) -> float:
+def evaluate_ppl(
+    model: torch.nn.Module, pad_token_id: int | None, testloader: DataLoader[dict[str, torch.Tensor]]
+) -> float:
     """
     Evaluate the model's perplexity on the test set using batch processing.
     It is expected that model is already on the correct device.
