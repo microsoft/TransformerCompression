3d9246a98025ca835d1445c6e66d2ee3510f150c
Pavel Myshkov <pmyshkov@microsoft.com>
2024-02-19 13:22:39 +0000

    Add variance based scheduler

--- src/slicegpt/slicing_scheduler.py
+++ src/slicegpt/slicing_scheduler.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from collections import defaultdict
 from typing import Callable, final
 
+import numpy as np
+
 from slicegpt.model_adapter import SlicingConfig
 
 
@@ -19,6 +21,11 @@ class SlicingScheduler(ABC):
     def __init__(self, *, do_slice_head: bool = False):
         self.slicing_conf: SlicingConfig = SlicingConfig()
         self.slicing_conf.do_slice_head = do_slice_head
+
+        # eigenvalues obtained from PCA
+        self.embedding_eigenvalues: list[float] = []
+        self.attention_eigenvalues: dict[int, list[float]] = {}
+        self.mlp_eigenvalues: dict[int, list[float]] = {}
 
     @property
     def do_slice_head(self) -> bool:
@@ -49,17 +56,23 @@ class SlicingScheduler(ABC):
     @final
     def get_embedding_dimensions(self) -> dict[int, int]:
         """Return the input embedding dimensions."""
-        val = self._get_input_embedding_dimensions()
+        if self.slicing_conf.embedding_dimensions:
+            return self.slicing_conf.embedding_dimensions
+
+        val = self._get_embedding_dimensions()
         self.slicing_conf.embedding_dimensions = val
         return val
 
     @abstractmethod
-    def _get_input_embedding_dimensions(self) -> dict[int, int]:
+    def _get_embedding_dimensions(self) -> dict[int, int]:
         raise NotImplementedError
 
     @final
     def get_attention_input_dimension(self, idx: int) -> int:
         """Return the attention input dimension for the specified layer index."""
+        if idx in self.slicing_conf.attention_input_dimensions:
+            return self.slicing_conf.attention_input_dimensions[idx]
+
         val = self._get_attention_input_dimension(idx)
         self.slicing_conf.attention_input_dimensions[idx] = val
         return val
@@ -69,12 +82,30 @@ class SlicingScheduler(ABC):
         raise NotImplementedError
 
     @final
-    def get_attention_output_dimension(self, idx, match_head_dim: bool) -> int:
+    def get_mlp_output_dimension(self, idx: int) -> int:
+        """Return the mlp output dimension for the specified layer index."""
+        if idx in self.slicing_conf.mlp_output_dimensions:
+            return self.slicing_conf.mlp_output_dimensions[idx]
+
+        use_head_dim = idx == self.layers_num - 1
+        val = self._get_mlp_output_dimension(idx) if not use_head_dim else self.get_head_dimension()
+        self.slicing_conf.mlp_output_dimensions[idx] = val
+        return val
+
+    @abstractmethod
+    def _get_mlp_output_dimension(self, idx: int) -> int:
+        raise NotImplementedError
+
+    @final
+    def get_attention_output_dimension(self, idx, match_head_dim: bool | None = None) -> int:
         """Return the attention output dimension for the specified layer index."""
         if self.parallel_blocks:
             return self.get_mlp_output_dimension(idx)
 
-        use_head_dim = match_head_dim and idx == self.layers_num - 1
+        if idx in self.slicing_conf.attention_output_dimensions:
+            return self.slicing_conf.attention_output_dimensions[idx]
+
+        use_head_dim = idx == self.layers_num - 1 and match_head_dim
         val = self._get_attention_output_dimension(idx) if not use_head_dim else self.get_head_dimension()
         self.slicing_conf.attention_output_dimensions[idx] = val
         return val
@@ -89,6 +120,9 @@ class SlicingScheduler(ABC):
         if self.parallel_blocks:
             return self.get_attention_input_dimension(idx)
 
+        if idx in self.slicing_conf.mlp_input_dimensions:
+            return self.slicing_conf.mlp_input_dimensions[idx]
+
         val = self._get_mlp_input_dimension(idx)
         self.slicing_conf.mlp_input_dimensions[idx] = val
         return val
@@ -98,20 +132,11 @@ class SlicingScheduler(ABC):
         raise NotImplementedError
 
     @final
-    def get_mlp_output_dimension(self, idx: int) -> int:
-        """Return the mlp output dimension for the specified layer index."""
-        use_head_dim = idx == self.layers_num - 1
-        val = self._get_mlp_output_dimension(idx) if not use_head_dim else self.get_head_dimension()
-        self.slicing_conf.mlp_output_dimensions[idx] = val
-        return val
-
-    @abstractmethod
-    def _get_mlp_output_dimension(self, idx: int) -> int:
-        raise NotImplementedError
-
-    @final
     def get_head_dimension(self) -> int:
         """Return the LM head dimension."""
+        if self.slicing_conf.head_dimension is not None:
+            return self.slicing_conf.head_dimension
+
         val = self._get_head_dimension() if self.slicing_conf.do_slice_head else self.hidden_size
         self.slicing_conf.head_dimension = val
         return val
@@ -119,6 +144,18 @@ class SlicingScheduler(ABC):
     @abstractmethod
     def _get_head_dimension(self) -> int:
         raise NotImplementedError
+
+    def set_embedding_eigenvalues(self, eigenvalues: list[float]) -> None:
+        """Set the eigenvalues of the embeddings PCA."""
+        self.embedding_eigenvalues = eigenvalues
+
+    def set_attention_eigenvalues(self, idx: int, eigenvalues: list[float]) -> None:
+        """Set the eigenvalues of the attention layer PCA."""
+        self.attention_eigenvalues[idx] = eigenvalues
+
+    def set_mlp_eigenvalues(self, idx: int, eigenvalues: list[float]) -> None:
+        """Set the eigenvalues of the MLP layer PCA."""
+        self.mlp_eigenvalues[idx] = eigenvalues
 
 
 class ConfigSlicingScheduler(SlicingScheduler):
@@ -128,20 +165,20 @@ class ConfigSlicingScheduler(SlicingScheduler):
         super().__init__()
         self.slicing_conf = config
 
-    def _get_input_embedding_dimensions(self) -> dict[int, int]:
+    def _get_embedding_dimensions(self) -> dict[int, int]:
         return self.slicing_conf.embedding_dimensions
 
     def _get_attention_input_dimension(self, idx: int) -> int:
         return self.slicing_conf.attention_input_dimensions[idx]
+
+    def _get_mlp_output_dimension(self, idx: int) -> int:
+        return self.slicing_conf.mlp_output_dimensions[idx]
 
     def _get_attention_output_dimension(self, idx: int) -> int:
         return self.slicing_conf.attention_output_dimensions[idx]
 
     def _get_mlp_input_dimension(self, idx: int) -> int:
         return self.slicing_conf.mlp_input_dimensions[idx]
-
-    def _get_mlp_output_dimension(self, idx: int) -> int:
-        return self.slicing_conf.mlp_output_dimensions[idx]
 
     def _get_head_dimension(self) -> int:
         return self.slicing_conf.head_dimension
@@ -154,19 +191,19 @@ class ConstSlicingScheduler(SlicingScheduler):
         super().__init__(do_slice_head=do_slice_head)
         self.dimension: int = dimension
 
-    def _get_input_embedding_dimensions(self) -> dict[int, int]:
+    def _get_embedding_dimensions(self) -> dict[int, int]:
         return defaultdict(lambda: self.dimension)
 
     def _get_attention_input_dimension(self, idx: int) -> int:
+        return self.dimension
+
+    def _get_mlp_output_dimension(self, idx: int) -> int:
         return self.dimension
 
     def _get_attention_output_dimension(self, idx: int) -> int:
         return self.dimension
 
     def _get_mlp_input_dimension(self, idx: int) -> int:
-        return self.dimension
-
-    def _get_mlp_output_dimension(self, idx: int) -> int:
         return self.dimension
 
     def _get_head_dimension(self) -> int:
@@ -186,13 +223,13 @@ class ForwardSlicingScheduler(SlicingScheduler, ABC):
     def _get_attention_input_dimension(self, idx: int) -> int:
         # return the input embedding dimension when at the first attn layer inputs
         if idx == 0:
-            return self._get_input_embedding_dimensions()[0]  # all dimensions are the same there
+            return self.get_embedding_dimensions()[0]  # all dimensions are the same there
 
-        return self._get_mlp_output_dimension(idx - 1)
+        return self.get_mlp_output_dimension(idx - 1)
 
     @final
     def _get_mlp_input_dimension(self, idx: int) -> int:
-        return self._get_attention_output_dimension(idx)
+        return self.get_attention_output_dimension(idx)
 
 
 class FunctionSlicingScheduler(ForwardSlicingScheduler):
@@ -222,7 +259,7 @@ class FunctionSlicingScheduler(ForwardSlicingScheduler):
         val -= val % self.round_interval
         return val
 
-    def _get_input_embedding_dimensions(self) -> dict[int, int]:
+    def _get_embedding_dimensions(self) -> dict[int, int]:
         return defaultdict(lambda: self._get_layer_dimension(0))
 
     def _get_attention_output_dimension(self, idx: int) -> int:
@@ -259,3 +296,38 @@ class FunctionSlicingScheduler(ForwardSlicingScheduler):
             round_interval=round_interval,
             do_slice_head=do_slice_head,
         )
+
+
+class ExplainedVarianceSlicingScheduler(ForwardSlicingScheduler):
+    """A slicing scheduler that applies sparsity based on the explained variance from the PCA."""
+
+    def __init__(
+        self,
+        *,
+        uev_threshold: float,
+        round_interval: int = 1,
+        do_slice_head: bool = False,
+    ):
+        super().__init__(do_slice_head=do_slice_head)
+        self.uev_threshold: float = uev_threshold
+        self.round_interval: int = round_interval
+
+    def _get_layer_dimension(self, eigen_vals: list[float], plot: bool = False) -> int:
+        eigen_vals = np.array(eigen_vals)
+        cum_var = np.cumsum(np.array(eigen_vals)) / np.sum(eigen_vals)
+        dim = np.argmax(cum_var > 1 - self.uev_threshold)
+        dim -= dim % self.round_interval
+        dim = int(dim)
+        return dim
+
+    def _get_embedding_dimensions(self) -> dict[int, int]:
+        return defaultdict(lambda: self._get_layer_dimension(self.embedding_eigenvalues))
+
+    def _get_attention_output_dimension(self, idx: int) -> int:
+        return self._get_layer_dimension(self.mlp_eigenvalues[idx])
+
+    def _get_mlp_output_dimension(self, idx: int) -> int:
+        return self._get_layer_dimension(self.attention_eigenvalues[idx])
+
+    def _get_head_dimension(self) -> int:
+        return self.get_attention_output_dimension(self.layers_num - 1)
