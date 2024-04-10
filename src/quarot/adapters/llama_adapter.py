5775fa9badad13ada80cd647cf2103008c10380f
Max Croci <t-mcroci@microsoft.com>
2024-04-10 10:58:32 +0000

    add adapters, layernorm fusion and hf_utils

---
+++ src/quarot/adapters/llama_adapter.py
@@ -0,0 +1,160 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+#
+# This file contains derivations from
+# https://github.com/huggingface/transformers/blob/main/src/transformers/models/llama/modeling_llama.py
+# Copyright 2022 EleutherAI and the HuggingFace Inc. team. All rights reserved.
+# https://www.apache.org/licenses/LICENSE-2.0
+from typing import cast
+
+import torch
+from torch import FloatTensor, LongTensor, Tensor, matmul
+from torch.nn import Linear, Module
+from transformers import PretrainedConfig, PreTrainedTokenizerBase
+from transformers.models.llama.modeling_llama import LlamaConfig, LlamaDecoderLayer, LlamaForCausalLM, LlamaRMSNorm
+
+from quarot.model_adapter import LayerAdapter, ModelAdapter
+from slicegpt.adapters.llama_adapter import LlamaLayerAdapter  # Llama layer adapter is the same as in SliceGPT
+
+
+class QuaRotLlamaDecoderLayer(LlamaDecoderLayer):
+    pass
+
+
+class LlamaModelAdapter(ModelAdapter):
+    def __init__(self, model: LlamaForCausalLM) -> None:
+        super().__init__()
+        self._model: LlamaForCausalLM = model
+
+    @property
+    def model(self) -> Module:
+        return self._model
+
+    @property
+    def config(self) -> PretrainedConfig:
+        return self._model.config
+
+    @property
+    def config_type(self) -> type:
+        return LlamaConfig
+
+    @property
+    def parallel_blocks(self) -> bool:
+        return False
+
+    @property
+    def seqlen(self) -> int:
+        return self.config.max_position_embeddings
+
+    @property
+    def hidden_size(self) -> int:
+        return self.config.hidden_size
+
+    @property
+    def should_bake_mean_into_linear(self) -> bool:
+        return False
+
+    @property
+    def original_layer_type(self) -> type:
+        return LlamaDecoderLayer
+
+    @property
+    def original_layer_norm_type(self) -> type:
+        return LlamaRMSNorm
+
+    @property
+    def layer_adapter_type(self) -> type:
+        return LlamaLayerAdapter
+
+    @property
+    def quarot_layer_type(self) -> type:
+        return QuaRotLlamaDecoderLayer
+
+    @property
+    def use_cache(self) -> bool:
+        return self.config.use_cache
+
+    @use_cache.setter
+    def use_cache(self, value: bool) -> None:
+        self.config.use_cache = value
+
+    def compute_output_logits(self, input_ids: Tensor) -> FloatTensor:
+        return self.model(input_ids=input_ids).logits
+
+    def convert_layer_to_quarot(self, layer: Module, layer_idx: int | None) -> Module:
+        quarot_layer = self.quarot_layer_type(cast(self.config_type, self.config), layer_idx).to(
+            self.config.torch_dtype
+        )
+        quarot_layer.load_state_dict(layer.state_dict(), strict=True)
+        return quarot_layer
+
+    def get_layers(self) -> list[LayerAdapter]:
+        return [self.layer_adapter_type(layer) for layer in self.model.model.layers]
+
+    def get_raw_layer_at(self, index: int) -> Module:
+        return self.model.model.layers[index]
+
+    def set_raw_layer_at(self, index: int, new_layer: Module) -> None:
+        self.model.model.layers[index] = new_layer
+
+    def get_embeddings(self) -> list[Module]:
+        return [self.model.model.embed_tokens]
+
+    def get_pre_head_layernorm(self) -> type:
+        pre_head_layernorm = self.model.model.norm
+        assert isinstance(pre_head_layernorm, self.original_layer_norm_type)
+        return pre_head_layernorm
+
+    def get_lm_head(self) -> Linear:
+        return self.model.lm_head
+
+    def post_init(self, tokenizer: PreTrainedTokenizerBase) -> None:
+        # Llama-2 doesn't have a pad token by default
+        tokenizer.pad_token = tokenizer.eos_token
+        self.config.pad_token_id = tokenizer.pad_token_id
+
+    @classmethod
+    def _from_pretrained(
+        cls,
+        model_name: str,
+        model_path: str,
+        *,
+        dtype: torch.dtype = torch.float16,
+        local_files_only: bool = False,
+        token: str | bool | None = None,
+    ) -> ModelAdapter | None:
+        if not model_name.startswith("meta-llama/Llama-2"):
+            return None
+
+        model = LlamaForCausalLM.from_pretrained(
+            model_path, torch_dtype=dtype, token=token, local_files_only=local_files_only
+        )
+        model.config.torch_dtype = dtype
+
+        return LlamaModelAdapter(model)
+
+    @classmethod
+    def _from_uninitialized(
+        cls,
+        model_name: str,
+        model_path: str,
+        *,
+        dtype: torch.dtype = torch.float16,
+        local_files_only: bool = False,
+        token: str | bool | None = None,
+    ) -> ModelAdapter | None:
+        if not model_name.startswith("meta-llama/Llama-2"):
+            return None
+
+        class UninitializedLlamaForCausalLM(LlamaForCausalLM):
+            def _init_weights(self, _) -> None:
+                # Prevent weight initialization
+                pass
+
+        config = LlamaConfig.from_pretrained(
+            model_path, torch_dtype=dtype, token=token, local_files_only=local_files_only
+        )
+        model = UninitializedLlamaForCausalLM(config)
+        model = model.to(dtype=dtype)
+
+        return LlamaModelAdapter(model)
