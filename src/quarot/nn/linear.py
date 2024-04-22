75019e53b4bf952ddb61c2604a347c44c95e27b2
Max Croci <t-mcroci@microsoft.com>
2024-04-22 11:40:48 +0000

    tidy up linear and add tests

--- src/quarot/nn/linear.py
+++ src/quarot/nn/linear.py
@@ -8,15 +8,14 @@ class QuarotFP16Linear(torch.nn.Module):
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.register_buffer('weight_scales', torch.zeros((self.out_features, 1), requires_grad=False))
+        self.register_buffer('weight_scales', torch.ones((self.out_features, 1), dtype=dtype))
         self.register_buffer('weight', torch.zeros((self.out_features, self.in_features), dtype=dtype))
         if bias:
             self.register_buffer('bias', torch.zeros((self.out_features), dtype=dtype))
         else:
             self.bias = None
 
-    def forward(self, x):
-        assert isinstance(x, PackedQuantizedTensor)  # Quantized input is given
+    def forward(self, x: PackedQuantizedTensor) -> torch.tensor:
         x, scales_x = x.quantized_x, x.scales_x
 
         # perform matmul with the emulated quantized tensors
@@ -31,31 +30,13 @@ class QuarotFP16Linear(torch.nn.Module):
         return x
 
     @staticmethod
-    def from_float(
+    def like(
         module: torch.nn.Linear,
-        weight_scales=None,
     ) -> 'QuarotFP16Linear':
         '''
-        Generate a new QuarotFP16Linear module from a FP16 Linear module.
-        The weight matrix should have the same shape as the weight matrix of the FP16 Linear module and rounded using torch.round()
-        routine. We will convert it and save it in the int_weight buffer.
+        Generate a new QuarotFP16Linear module with the same shapes & bias flag as a given Linear module.
         '''
-        weight_matrix = module.weight.data
-        quarot_fp16_module = QuarotFP16Linear(
-            module.in_features, module.out_features, bias=module.bias is not None, dtype=weight_matrix.dtype
-        ).to(weight_matrix.dtype)
-
-        if weight_scales is not None:
-            assert weight_scales.shape == (module.out_features, 1), 'weight_scales should have shape (out_features, 1)'
-            weight_matrix = weight_matrix.cuda()
-            quarot_fp16_module.weight_scales.copy_(weight_scales.to(weight_matrix.dtype))
-            int_rounded_weight = (weight_matrix / weight_scales.cuda()).round()
-            quarot_fp16_module.weight.copy_(int_rounded_weight.cpu())
-
-            if module.bias is not None:
-                quarot_fp16_module.bias.copy_(module.bias)
-
-        return quarot_fp16_module
+        return QuarotFP16Linear(module.in_features, module.out_features, bias=module.bias is not None)
 
 
 # class Linear4bit(torch.nn.Module):
