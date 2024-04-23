dcc8f4890d715ca32f0539c47e95e4bb596ae2a9
Pashmina Cameron <pcameron@microsoft.com>
2024-04-23 13:40:35 +0100

    Formatting module imports

--- src/slicegpt/__init__.py
+++ src/slicegpt/__init__.py
@@ -4,8 +4,8 @@
 from .adapters.llama_adapter import LlamaModelAdapter
 from .adapters.opt_adapter import OPTModelAdapter
 from .adapters.phi2_adapter import Phi2ModelAdapter
-from .adapters.sliced_phi import SlicedPhi2Config, SlicedPhi, SlicedPhiForCausalLM
-from .adapters.sliced_llama import SlicedLlamaConfig, SlicedLlama, SlicedLlamaForCausalLM
+from .adapters.sliced_llama import SlicedLlama, SlicedLlamaConfig, SlicedLlamaForCausalLM
+from .adapters.sliced_phi import SlicedPhi, SlicedPhi2Config, SlicedPhiForCausalLM
 from .data_utils import get_dataset, prepare_dataloader
 from .gpu_utils import benchmark, distribute_model, evaluate_ppl
 from .hf_utils import get_model_and_tokenizer, load_sliced_model
