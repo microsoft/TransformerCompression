6210fc1953d9f73356c3c4ea37e2f578eed1b090
Liana Mikaelyan <mvl1ana98@gmail.com>
2024-04-23 16:32:47 +0000

    Add intermediate_size to OPT adapter to fix tests

--- src/slicegpt/adapters/opt_adapter.py
+++ src/slicegpt/adapters/opt_adapter.py
@@ -176,6 +176,10 @@ class OPTModelAdapter(ModelAdapter):
     @property
     def hidden_size(self) -> int:
         return self.config.hidden_size
+    
+    @property
+    def intermediate_size(self) -> int:
+        return self.config.intermediate_size
 
     @property
     def should_bake_mean_into_linear(self) -> bool:
