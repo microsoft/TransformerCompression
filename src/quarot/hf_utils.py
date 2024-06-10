5f129537e91af7d9a3b6a14b89789dd34048dc51
James Hensman <jameshensman@microsoft.com>
2024-06-10 15:23:51 +0100

    llama3 in hf-utils

--- src/quarot/hf_utils.py
+++ src/quarot/hf_utils.py
@@ -17,7 +17,7 @@ from .modeling_phi3 import QuarotPhi3Config, QuarotPhi3ForCausalLM
 def quarot_model_config(
     model_name_or_path: str, dtype: torch.dtype, groupsize: int | None = None, offset: bool = False
 ):
-    if model_name_or_path in ['meta-llama/Llama-2-7b-hf', 'meta-llama/Llama-2-13b-hf']:
+    if model_name_or_path in ['meta-llama/Llama-2-7b-hf', 'meta-llama/Llama-2-13b-hf', 'meta-llama/Meta-Llama-3-8B']:
         model_config = QuarotLlamaConfig.from_pretrained(model_name_or_path, dtype=dtype, use_cache=False)
     elif model_name_or_path == 'microsoft/Phi-3-mini-4k-instruct':
         model_config = QuarotPhi3Config.from_pretrained(model_name_or_path, dtype=dtype, use_cache=False)
@@ -40,7 +40,7 @@ def get_quarot_model(
     online_had_mlp = True if rotate else False
     online_had_attn = True if rotate else False
     rms_norm = True if rotate else False
-    if model_name_or_path in ['meta-llama/Llama-2-7b-hf', 'meta-llama/Llama-2-13b-hf']:
+    if model_name_or_path in ['meta-llama/Llama-2-7b-hf', 'meta-llama/Llama-2-13b-hf', 'meta-llama/Meta-Llama-3-8B']:
         return QuarotLlamaForCausalLM(
             online_had_mlp=online_had_mlp,
             online_had_attn=online_had_attn,
