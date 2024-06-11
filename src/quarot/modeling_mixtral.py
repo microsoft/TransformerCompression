e94c076583cb6c5836a8648f6235446f0b3ab68e
Max Croci <t-mcroci@microsoft.com>
2024-06-11 12:44:53 +0000

    full file

--- src/quarot/modeling_mixtral.py
+++ src/quarot/modeling_mixtral.py
@@ -25,6 +25,10 @@ from slicegpt.modules import RMSN
 from .nn import OnlineHadamard, QuarotFP16Linear
 from .nn.quantizer import ActQuantizer, DummyActQuantizer, KVQuantizerDequantizer
 
+try:
+    from transformers.models.mistral.modeling_mistral import _flash_supports_window_size
+except ImportError:
+    _flash_supports_window_size = False
 
 class QuarotMixtralConfig(MixtralConfig):
     model_type = "mixtral_quarot"
@@ -161,7 +165,6 @@ class QuarotMixtralFlashAttention2(MixtralFlashAttention2):
         output_attentions: bool = False,
         use_cache: bool = False,
     ):
-
         bsz, q_len, _ = hidden_states.size()
 
         # QuaRot: quantize hidden states at input of attention
@@ -171,15 +174,68 @@ class QuarotMixtralFlashAttention2(MixtralFlashAttention2):
         key_states = self.k_proj(hidden_states)
         value_states = self.v_proj(hidden_states)
 
+        # Flash attention requires the input to have the shape
+        # batch_size x seq_length x head_dim x hidden_dim
+        # therefore we just need to keep the original shape
         query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim)  # QuaRot: remove transpose
         key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim)  # QuaRot: remove transpose
         value_states = value_states.view(
             bsz, q_len, self.num_key_value_heads, self.head_dim
         )  # QuaRot: remove transpose
 
+        kv_seq_len = key_states.shape[-3] # QuaRot: get sequence length
+
         # Because the input can be padded, the absolute sequence length depends on the max position id.
         rotary_seq_len = max(kv_seq_len, position_ids[:, -1].max().item()) + 1
         cos, sin = self.rotary_emb(value_states, seq_len=rotary_seq_len)
+        
+        query_states, key_states = apply_rotary_pos_emb(
+            query_states, key_states, cos, sin, unsqueeze_dim=2
+        )  # QuaRot: requires unsqueeze
+
+        # QuaRot: apply online hadamard to queries and keys
+        if self.online_had:
+            query_states = self.online_q_hadamard(query_states)
+            key_states = self.online_k_hadamard(key_states)
+
+        # QuaRot: quantize and dequantize keys and values
+        key_states = self.k_quantizer(key_states)
+        value_states = self.v_quantizer(value_states)
+
+        use_sliding_windows = (
+            _flash_supports_window_size
+            and getattr(self.config, "sliding_window", None) is not None
+            and kv_seq_len > self.config.sliding_window
+        )
+
+        torch.repeat_interleave(key_states, dim=2, repeats=self.num_key_value_groups)
+        torch.repeat_interleave(value_states, dim=2, repeats=self.num_key_value_groups)
+        dropout_rate = 0.0 if not self.training else self.attention_dropout
+
+        attn_output = self._flash_attention_forward(
+            query_states,
+            key_states,
+            value_states,
+            attention_mask,
+            q_len,
+            dropout=dropout_rate,
+            use_sliding_windows=use_sliding_windows,
+        )
+
+        # QuaRot: apply online hadamard if needed
+        if self.online_had:
+            attn_output = self.online_o_proj_hadamard(attn_output.transpose(-1, -2)).transpose(-1, -2)
+
+        attn_output = attn_output.reshape(bsz, q_len, self.hidden_size).contiguous()
+
+        # QuaRot: quantize inputs of output projection
+        attn_output = self.o_proj_input_quantizer(attn_output)
+        attn_output = self.o_proj(attn_output)
+
+        if not output_attentions:
+            attn_weights = None
+
+        return attn_output, attn_weights, past_key_value
 
 
 class QuarotMixtralForCausalLM(MixtralForCausalLM):
@@ -212,6 +268,21 @@ class QuarotMixtralForCausalLM(MixtralForCausalLM):
             self.model.norm = RMSN(config.hidden_size, eps=config.rms_norm_eps)
 
         for layer_idx, layer in enumerate(self.model.layers):
+            layer.self_attn = QuarotMixtralFlashAttention2(
+                config=config,
+                act_bits=act_bits,
+                act_clip_ratio=act_clip_ratio,
+                act_groupsize=act_groupsize,
+                k_bits=k_bits,
+                k_clip_ratio=k_clip_ratio,
+                k_groupsize=k_groupsize,
+                v_bits=v_bits,
+                v_clip_ratio=v_clip_ratio,
+                v_groupsize=v_groupsize,
+                online_had=online_had_attn,
+                layer_idx=layer_idx,
+            )
+
             layer.block_sparse_moe = QuarotMixtralSparseMoeBlock(
                 config=config,
                 act_bits=act_bits,
